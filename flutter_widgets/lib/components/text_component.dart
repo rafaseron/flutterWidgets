1f9e63e59ed519eef3635cf919fa12ab844ad8d9
rafaseron <rafaelseron3011@gmail.com>
2024-11-29 15:41:44 -0400

    initial commit

---
+++ flutter_widgets/lib/components/text_component.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyCustomText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Hello PUCPR!",
+      style: TextStyle(fontSize: 24, color: Colors.red),
+    );
+  }
+}
