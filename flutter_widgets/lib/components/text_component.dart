e068050d3ad93e20b35346d1b12c4f6187a39c89
rafaseron <rafaelseron3011@gmail.com>
2024-11-29 17:00:22 -0400

    feat: Widget de Texto

--- flutter_widgets/lib/components/text_component.dart
+++ flutter_widgets/lib/components/text_component.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyCustomText extends StatelessWidget {
+  const MyCustomText({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Text(
