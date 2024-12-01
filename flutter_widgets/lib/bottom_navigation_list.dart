58a84ebee5704a2d895189e085416ee850381764
rafaseron <rafaelseron3011@gmail.com>
2024-12-01 14:50:11 -0400

    feat: BottomBarItemList

---
+++ flutter_widgets/lib/bottom_navigation_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+BottomNavigationBarItem myItem = const BottomNavigationBarItem(
+  icon: Icon(Icons.home),
+  label: 'Home',
+);
+
+List<BottomNavigationBarItem> myItemList = [myItem, myItem, myItem];
